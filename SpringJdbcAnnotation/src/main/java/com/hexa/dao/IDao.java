03830b9ae9274915b0bba7d0cd03a7c4efd92a5a
SaiharshiniP-hexaware <35030615+SaiharshiniP-hexaware@users.noreply.github.com>
2018-03-30 18:39:52 +0530

    Add files via upload

---
+++ SpringJdbcAnnotation/src/main/java/com/hexa/dao/IDao.java
@@ -0,0 +1,16 @@
+package com.hexa.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hexa.entity.Student;
+
+public interface IDao {
+	List<Map<String,Object>> getStudentsMap(String dept);
+	Map<String,Object> getStudentAsMap(int sid);
+	int updateMarks(int sid,int marks);
+	List<Student> getStudents(String dept);
+	Student getStudent(int sid);
+	
+
+}
